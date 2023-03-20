719af8a7b0bc1aeb91816c724c412ae7e3e400c4
Grace Boaz <anna.g.boaz@gmail.com>
2023-03-20 09:43:03 -0500

    added a basic navBar

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -5,19 +5,33 @@ export const NavBar = () => {
     const navigate = useNavigate()
     return (
         <ul className="navbar">
-            <li className="navbar__item active">
-                <Link className="navbar__link" to="/tickets">Tickets</Link>
+            <li className="navbar__item">
+                <Link className="nav-link" to="/knit">Knit</Link>
+            </li>
+            <li className="navbar__item">
+                <Link className="nav-link" to="/crochet">Crochet</Link>
+            </li>
+            <li className="navbar__item">
+                <Link className="nav-link" to="/home">home</Link>
             </li>
             {
-                localStorage.getItem("honey_user")
-                    ? <li className="navbar__item navbar__logout">
-                        <Link className="navbar__link" to="" onClick={() => {
-                            localStorage.removeItem("honey_user")
-                            navigate("/", {replace: true})
-                        }}>Logout</Link>
-                    </li>
-                    : ""
-            }
-        </ul>
+                (localStorage.getItem("cc_token") !== null) ?
+                    <li className="nav-item">
+                        <button className="nav-link fakeLink"
+                            onClick={() => {
+                                localStorage.removeItem("cc_token")
+                                navigate('/login')
+                            }}
+                        >Logout</button>
+                    </li> :
+                    <>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/login">Login</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/register">Register</Link>
+                        </li>
+                    </>
+            }        </ul>
     )
 }
--- src/components/projectSingle/ProjectSingle.js
+++ src/components/projectSingle/ProjectSingle.js
@@ -2,17 +2,15 @@ import { useState, useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { getSingleProject } from "../../managers/ProjectManager";
 
+/*  
+    when the user clicks on a project, they can
+    see the project in a single view 
+*/
+
 export const ProjectSingle = () => {
 
-    /* when the user clicks on a project, they can get a closer see
-       the project in a single view 
-        - get a single project by accessing the Id with uesParams
-          and passing that id into the getSingleProject method
-        - invoke useEffect to ensure that project displays ad soon as the 
-          page is rendered
-    */
-
     const navigate = useNavigate()
+    //get a single project by accessing the Id with uesParams...
     const { projectId } = useParams()
 
     const [currentProject, setCurrentProject] = useState({
@@ -26,6 +24,7 @@ export const ProjectSingle = () => {
     })
 
     useEffect(() => {
+        //...and passing that id into the getSingleProject method
         getSingleProject(projectId)
         .then((projectData) => { setCurrentProject(projectData)})
     }, [projectId])
@@ -34,6 +33,7 @@ export const ProjectSingle = () => {
         <article className="project">
             <div className="project--name">{currentProject.name}</div>
             <div className="project--toolSize">{currentProject.tool_size}</div>
+            <div className="project--toolSize">{currentProject.photo}</div>
             <div className="project--directions">{currentProject.directions_link}</div>
             <div className="project--patternType">{currentProject.pattern_type}</div>
         </article>
