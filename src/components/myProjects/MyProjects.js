a0dfca3f4d7f776e21a4ad763feacdcc2910a432
Grace Boaz <anna.g.boaz@gmail.com>
2023-03-22 00:39:04 -0500

    need to get the Id of the current user.

--- src/components/myProjects/MyProjects.js
+++ src/components/myProjects/MyProjects.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef, useState } from "react"
 import { Link, useNavigate } from "react-router-dom"
-import { getCritterProjects } from "../../managers/ProjectManager"
+import { deleteProject, getCritterProjects, getProjects } from "../../managers/ProjectManager"
 
 export const MyProjects = (props) => {
     const navigate = useNavigate()
@@ -8,10 +8,14 @@ export const MyProjects = (props) => {
     const [ projects, setProjects] = useState([])
 
     //set the projects array from useState equal to the projects being fetched by the manager
+    //What is the easiest way to get the Id of the current user?
     useEffect(() => {
         getCritterProjects(1).then(data => setProjects(data))
     }, [])
 
+    const handleDelete = (projectId) => {
+        deleteProject(projectId).then(getProjects().then(data => setProjects(data)))
+    }
 
     return (
         <article className="projects">
@@ -23,6 +27,14 @@ export const MyProjects = (props) => {
                         <div className="project--photo">{project.photo}</div>
                         <div className="project--directions">directions: {project.directions_link}</div>
                         <div className="project--patternType">pattern: {project.pattern_type}</div>
+
+                        <button className="btn btn-1 btn-sep icon-create"
+                            onClick={() => {navigate({ pathname: `/projectedit/${project.id}`})}}>Edit
+                        </button>
+
+                        <button className="btn btn-3 btn-sep icon-create"
+                            onClick={() => {handleDelete(project.id)}}>Delete
+                        </button>
                     </section> 
                 })
             
